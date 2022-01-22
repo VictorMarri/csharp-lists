39e45a77c5cfa534b39f2a0f5ae7c49926f49c4c
Victor Marri <victor.marri@dtidigital.com.br>
2022-01-22 11:10:27 -0300

    Dictionary

---
+++ Collections/Dictionary/DictionaryLearn.cs
@@ -0,0 +1,52 @@
+﻿using CollectionsLearn.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsLearn.Collections.Dictionary
+{
+    public static class DictionaryLearn
+    {
+        static IDictionary<string, Trabalhador> trabalhadores = new Dictionary<string, Trabalhador>();
+
+        //Dictionaries não respeitam uma ordem especifica, ele organiza as chaves de forma não ordenada,
+        //portanto, a posição que cada chave ocupa na memoria nçao depende da ordem de inserção.
+        //Geralmente, se voce apaga um elemento da lista
+        //E logo em seguida insere outro, este outro ficará no lugar do antigo item excluido
+
+        public static void InicializaDictionary()
+        {
+            adicionarTrabalhadores(trabalhadores);
+            imprimirTrabalhadores(trabalhadores);
+            testarOrdenaçãoDeDictionary(trabalhadores);
+        }
+
+        private static void testarOrdenaçãoDeDictionary(IDictionary<string, Trabalhador> trabalhadores)
+        {
+            trabalhadores.Remove("AM");
+            trabalhadores.Add("CC", new Trabalhador("Caio Costa", 1356));
+            Console.WriteLine("Aperte uma tecla para o console limpar...");
+            Console.ReadKey(true);
+            Console.Clear();
+            imprimirTrabalhadores(trabalhadores); //Caio costa vai ficar exatamente no mesmo lugar que Amanda Maia
+        }
+
+        private static void imprimirTrabalhadores(IDictionary<string, Trabalhador> trabalhadores)
+        {
+            foreach (var trabalhador in trabalhadores)
+            {
+                Console.WriteLine(trabalhador);
+            }
+        }
+
+        private static void adicionarTrabalhadores(IDictionary<string, Trabalhador> trabalhadores)
+        {
+            trabalhadores.Add("CH", new Trabalhador("Carlos Henrique", 3458));
+            trabalhadores.Add("AM", new Trabalhador("Amanda Maia", 8965));
+            trabalhadores.Add("JH", new Trabalhador("Jorge Henrique", 8877));
+            trabalhadores.Add("VM", new Trabalhador("Víctor Marri", 4444));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using CollectionsLearn.Collections;
+using CollectionsLearn.Collections.Dictionary;
 using CollectionsLearn.Collections.LinkedList;
 using CollectionsLearn.Collections.Lista;
 using CollectionsLearn.Collections.Sets;
@@ -10,39 +11,41 @@ namespace CollectionsLearn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Escolha sobre qual estrutura de dados você quer operar:");
-            Console.WriteLine("1 - Array\n2 - Lists\n3 - ObjectList\n4 - Sets\n5 - LinkedList");
-            int escolhaEstrutura = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Escolha sobre qual estrutura de dados você quer operar:");
+            //Console.WriteLine("1 - Array\n2 - Lists\n3 - ObjectList\n4 - Sets\n5 - LinkedList");
+            //int escolhaEstrutura = int.Parse(Console.ReadLine());
 
-            switch (escolhaEstrutura)
-            {
-                case 1:
-                    Console.Clear();
-                    ArraysLearn.Arrays();
-                break;
+            //switch (escolhaEstrutura)
+            //{
+            //    case 1:
+            //        Console.Clear();
+            //        ArraysLearn.Arrays();
+            //    break;
 
-                case 2:
-                    Console.Clear();
-                    ListsLearn.Lists();
-                break;
+            //    case 2:
+            //        Console.Clear();
+            //        ListsLearn.Lists();
+            //    break;
 
-                case 3:
-                    Console.Clear();
-                    ObjectListLearn.ListaObjetos();
-                break;
+            //    case 3:
+            //        Console.Clear();
+            //        ObjectListLearn.ListaObjetos();
+            //    break;
 
-                case 4:
-                    Console.Clear();
-                    SetsLearn.IniciaSets();
-                break;
+            //    case 4:
+            //        Console.Clear();
+            //        SetsLearn.IniciaSets();
+            //    break;
 
-                case 5:
-                    Console.Clear();
-                    LinkedListLearning.Inicializar();
-                break;
-            }
+            //    case 5:
+            //        Console.Clear();
+            //        LinkedListLearning.Inicializar();
+            //    break;
+            //}
 
-            Console.ReadKey(true);
+            //Console.ReadKey(true);
+
+            DictionaryLearn.InicializaDictionary();
         }
 
     }
